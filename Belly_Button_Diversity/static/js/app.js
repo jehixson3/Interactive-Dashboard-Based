d578be045acad7ee2825244fad46a95c992af839
John Hixson <johnhixson2020@u.northwestern.edu>
2019-12-02 09:34:53 -0600

    My data stopped loading

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -1,68 +1,78 @@
-
 function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
-  // Use `d3.json` to fetch the metadata for a sample
+ // Use `d3.json` to fetch the metadata for a sample
   var url = `/metadata/${sample}`;
   d3.json(url).then(function(sample){
-
+   
     // Use d3 to select the panel with id of `#sample-metadata`
     var sample_metadata = d3.select("#sample-metadata");
-
     // Use `.html("") to clear any existing metadata
     sample_metadata.html("");
-
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata.
-    Object.entries(sample).forEach(function ([key, value]) {
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.defineProperties(sample).forEach(function ([key, value]) {
       var row = sample_metadata.append("panel-body");
       row.text(`${key}: ${value} \n`);
     });
   });
 }
-
 function buildCharts(sample) {
-
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var url = `/samples/${sample}`;
-  d3.json(url).then(function (response){
-    // if (error) return console.log(error);
-    var x_value = response.otu_ids;
-    var y_value = response.sample_values;
-    var size_value = response.sample_values;
-    var label = response.otu_labels;
-    console.log(x_value);
-    console.log(y_value);
-    console.log(labels);
+  var url = `/metadata/${sample}`;
+  d3.json(url).then(function(data){
     // @TODO: Build a Bubble Chart using the sample data
+    var x_values = data.otu_ids;
+    var y_values = data.sample_values;
+    var size_value = data.sample_values;
+    var label = data.out_labels;
     var trace1 = {
-      x: x_value,
-      y: y_value,
-      mode:"markers", 
+      x: x_values,
+      y: y_values,
+      text: label,
+      mode: "markers",
       marker:{
         size: size_value,
-        color: x_value,
+        color: x_values,
         colorscale: "Rainbow",
         labels: label,
-        type: 'scatter',
-        opacity: 0.3
+        type: 'scatter'
       }
     };
-  
-    var data1 = [trace1];
+    var data =[trace1];
 
     var layout = {
-      title: 'Marker Size',
-      xaxis: { title: 'OTU ID' },
-      showlegend: true
+      margin: { t: 30, b: 100},
+      xaxis: { title: "OTU ID"},
     };
+
+      Plotly.newPlot('bubble', data, layout);
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+    d3.json(url).then(function(data) {  
+        var pie_values = data.sample_values.slice(0,10);
+        var pie_labels = data.otu_ids.slice(0,10);
+        var pie_hover = data.otu_labels.slice(0,10);
+
+        var data = [{
+          values: pie_values,
+          labels: pie_labels,
+          hovertext: pie_hover,
+          type: 'pie'
+        }];
+
+        Plotly.newPlot('pie', data);
+
+      });
   });
-    Plotly.newPlot("bubble", data1, layout); 
 }
+
 
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
@@ -71,19 +81,16 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
 }
-
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
-
 // Initialize the dashboard
 init();
